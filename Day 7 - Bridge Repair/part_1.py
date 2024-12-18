f6b13567f25449eb371356dc219e5249faa6524e
Ramon Walther <Ramon@Walther.codes>
2024-12-18 23:53:51 +0100

    Added final solution for day 7

---
+++ Day 7 - Bridge Repair/part_1.py
@@ -0,0 +1,53 @@
+################################################
+# Day 7: Bridge Repair - Part 1 (by RawbitDev) #
+################################################
+
+import os
+import itertools
+from typing import List
+
+in_file = lambda filename : os.path.join(os.path.dirname(__file__), "data", filename)
+
+#####################################################
+
+
+def get_equations(filepath: str) -> tuple[int, List[int]]:
+    equations = []
+    with open(filepath, encoding="utf8") as file:
+        for line in file:
+            line = line.strip()
+            if line:
+                splitted = line.split(": ")
+                equations.append((int(splitted[0]), [int(n) for n in splitted[1].split(" ")]))
+    return equations
+
+
+def is_equation_possible(equation) -> bool:
+    num_ops = len(equation[1]) - 1
+    combinations = itertools.product(["*", "+"], repeat=num_ops)
+    for comb in combinations:
+        calc = equation[1][0]
+        for i, op in enumerate(comb, start=1):
+            if op == "*":
+                calc *= equation[1][i]
+            elif op == "+":
+                calc += equation[1][i]
+        if calc == equation[0]:
+            return True
+    return False
+
+
+def sum_possible_tests(filepath: str) -> int:
+    equations = get_equations(filepath)
+    sum_possible_test_values = 0
+    for equation in equations:
+        if is_equation_possible(equation):
+            sum_possible_test_values += equation[0]
+    return sum_possible_test_values
+
+
+if __name__ == "__main__":
+    assert(sum_possible_tests(in_file("test")) == 3749)
+
+    total_calibration_result = sum_possible_tests(in_file("input"))
+    print(f"The total calibration result of the possible equations is {total_calibration_result}.")
